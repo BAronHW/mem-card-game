d2662741b54422c5d055993784b1cab0e9292137
Aaron Wan (溫昊霖） <63551617+BAronHW@users.noreply.github.com>
2024-06-18 14:31:49 +0800

    made some small changes

--- src/components/GetPokemon.js
+++ src/components/GetPokemon.js
@@ -1,31 +1,31 @@
-export async function get10pokemon() {
-    async function fetchUniquePokemon() {
-        let pokemonArray = [];
-        for (let i = 1; i <= 10; i++) {
-            const singlePokemon = await getsinglepokemon();
-            pokemonArray.push(singlePokemon);
-        }
-
-        if (checkAllUnique(pokemonArray)) {
-            return pokemonArray;
-        } else {
-            return fetchUniquePokemon(); 
-        }
-    }
-
-    return fetchUniquePokemon();
-}
-
-function checkAllUnique(data) {
-    let set = new Set();
-    data.forEach(pokemon => set.add(pokemon.forms[0].name));
-    return set.size === data.length;
-}
-
-async function getsinglepokemon() {
-    const maxPokemonNum = 150;
-    let rand = Math.floor((Math.random() * maxPokemonNum) + 1);
+export default async function getsinglepokemon() {
+    const maxpokemonnum = 150;
+    let rand = Math.floor((Math.random() * maxpokemonnum) + 1);
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${rand}`);
-    const singlePokemon = await response.json();
-    return singlePokemon;
+    const singlepokemon = await response.json();
+    return singlepokemon;
 }
+
+export function checkAllUnique(array){
+    let set = new Set();
+    array.forEach(element => {
+        set.add(element);
+    });
+    return(set.size==array.length)
+
+}
+
+export async function get10pokemon(){
+    const pokemonarray = [];
+    for(let i=1;i<=10;i++){
+        const singlepokemon = await getsinglepokemon();
+        pokemonarray.push(singlepokemon);
+    }
+    if(checkAllUnique(pokemonarray)){
+        return pokemonarray;
+    }else{
+        return get10pokemon();
+    }
+}
+  
+  
